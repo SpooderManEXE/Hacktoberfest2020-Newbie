19809feff45bf0d0211e3c249a04b78c02c6f493
sk72codes <72934562+sk72codes@users.noreply.github.com>
2021-10-01 21:45:20 +0530

    Create sk72codes.c

---
+++ C/sk72codes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Sree\n");
+     printf("Age    : 22\n");
+     printf("Username : sk72codes\n");
+     return(0);
+  }
