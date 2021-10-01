d8cc6f404c3079e74b12b6d464cae6b80c613719
Sreelakshmi S <56115162+Shreelakshmi07@users.noreply.github.com>
2021-10-01 10:41:55 +0530

    Create Shreelakshmi07.c

---
+++ C/Shreelakshmi07.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Sreelakshmi S\n");
+     printf("Age    : 21\n");
+     printf("Username : Shreelakshmi07\n");
+     return(0);
+  }
